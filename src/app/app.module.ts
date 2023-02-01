0b259cca6ffa204d779b3cf3f4be6bdc39001044
illika <steve.milan7@gmail.com>
2023-02-01 17:53:38 -0500

    Modulo - Components a usar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/button';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,ButtonModule
+    BrowserModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
