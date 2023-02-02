b5cae791d07d480f180f1cbe3a7f5268ae03f855
illika <steve.milan7@gmail.com>
2023-02-01 19:46:10 -0500

    upper-lower-title case

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+
+  nombreLower: string = "illika";
+  nombreUpper: string = "ILLIKA";
+  nombreCompleto: string = "iLliKa pA. vZ.";
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
